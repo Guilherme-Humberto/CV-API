61ddcbe527a30e515f1c3921f610527e36e88aab
Guilherme Humberto <gbreu@outlook.com>
2020-09-09 00:15:13 -0300

    Model de Usuários

---
+++ src/models/Users.js
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose'
+import  crypto from 'crypto'
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        set: value => 
+            crypto
+                .createHash("md5")
+                .update(value)
+                .digest("hex")
+    },
+    age: {
+        type: String,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true
+    }
+})
+
+const User = model("User", UserSchema)
+export { User }
