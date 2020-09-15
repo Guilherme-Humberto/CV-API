76b19de7891800ee495504b3ae02387c14745603
Guilherme Humberto <gbreu@outlook.com>
2020-09-14 23:40:46 -0300

    Conexão com banco de dados

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,8 @@ import 'dotenv/config'
 
 connect(process.env.DB_CONNECTION, {  
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
 .then(() => console.log("Conectado ao banco de dados"))
 .catch(err => console.log(`Erro ao conectar ao banco de dados ${err}`))
