8bba973d6720d34216705a5af50114c7f64b4622
Guilherme Humberto <gbreu@outlook.com>
2020-09-14 23:41:10 -0300

    Configuração das rotas

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ import express from 'express'
 const router = express.Router()
 import controller from './controllers/User/UserController'
 import session from './controllers/User/UserSessionController'
+import historic from './controllers/UserLogged/LoggedController'
 
 router.post("/register", controller.create)
 router.post("/login", session.authenticated)
+router.post("/historic", historic.register)
+router.get("/historic/list", historic.listar)
 
 export { router }
