e0394dd683a079cf6a61ad4b8cff8a1b4091949f
Guilherme Humberto <gbreu@outlook.com>
2020-10-03 19:56:14 -0300

    Rotas

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ import controller from './controllers/User/UserController'
 import session from './controllers/User/UserSessionController'
 import historic from './controllers/UserLogged/LoggedController'
 
+router.get("/listUser", controller.listUser)
 router.post("/register", controller.create)
+router.put("/editUser/:id", controller.editUser)
 router.post("/login", session.authenticated)
-router.post("/historic", historic.register)
+router.post("/historic/:id", historic.register)
 router.get("/historic/list", historic.listar)
 
 export { router }
