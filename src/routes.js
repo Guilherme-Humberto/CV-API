6f199149773cd5261498412dc1683b9d1bc06593
Guilherme-Humberto <gbreu@outlook.com>
2020-10-27 15:46:38 -0300

    Rotas

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 import controller from './controllers/User/UserController'
 import session from './controllers/User/UserSessionController'
-import historic from './controllers/UserLogged/LoggedController'
+import historic from './controllers/Historic/HistoricController'
 
 router.get("/listUser", controller.listUser)
 router.post("/register", controller.create)
@@ -10,5 +10,6 @@ router.put("/editUser/:id", controller.editUser)
 router.post("/login", session.authenticated)
 router.post("/historic/:user_id", historic.register)
 router.get("/historic/list/:user_id", historic.listar)
+// router.delete("/historic/:user_id", historic.destroy)
 
 export { router }
