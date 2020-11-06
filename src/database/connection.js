d3a766b307a1099a71002338178d8ee9180c543b
Guilherme-Humberto <gbreu@outlook.com>
2020-11-06 15:59:31 -0300

    Conexão com banco de dados

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,7 +5,7 @@ connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: true
+    useFindAndModify: false
 })
 .then(() => console.log("Conectado ao banco de dados"))
 .catch(err => console.log(`Erro ao conectar ao banco de dados ${err}`))
