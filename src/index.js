d151ec6224e3cade2d972ebe0f70e123c776b644
Guilherme-Humberto <gbreu@outlook.com>
2020-11-06 16:00:47 -0300

    Configurações do servidor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 const app = express()
 import bodyparser from 'body-parser'
 import cors from 'cors'
+import path from 'path'
 import { router } from './routes'
 
 import './database/connection'
@@ -12,5 +13,6 @@ app.use(router)
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: false }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
 
 export { app }
